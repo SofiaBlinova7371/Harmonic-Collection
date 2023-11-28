a50d3a9aaf04f788f841c6ab68515c964931a854
SofiaBlinova7371 <143515417+SofiaBlinova7371@users.noreply.github.com>
2023-11-28 10:38:37 -0500

    Add files via upload

---
+++ Interaction_Final Files/Sadness/script.js
@@ -0,0 +1,12 @@
+var GFG = document.getElementById("audio");
+let vid = document.getElementById("video");
+
+function play() {
+  vid.play();
+  GFG.play();
+}
+
+function pause() {
+  vid.pause();
+  GFG.pause();
+}
